07aa979fbdc6d5978ddaeaa2b6e63164887a906b
SNValadez <snvaladez13@gmail.com>
2021-11-11 11:51:48 -0800

    add handlebars

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3003;
 
+app.engine("handlebars", exphbs ({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//
 
 
-
-
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT);
+  });
 

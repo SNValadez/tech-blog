a5a3d5c72f76399fa8311d43810e4d62fe1523ef
SNValadez <snvaladez13@gmail.com>
2021-11-11 11:24:26 -0800

    require express and handlebars

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+
+
+
+
+

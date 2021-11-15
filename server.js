219cdfa534c8d4a56bff447bfc5325a4a5165a61
SNValadez <snvaladez13@gmail.com>
2021-11-14 18:14:04 -0800

    add app.use

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3003;
@@ -9,6 +10,8 @@ app.engine("handlebars", exphbs ({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 //
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log('App listening on PORT ' + PORT);

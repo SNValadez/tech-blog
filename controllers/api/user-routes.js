5726b18118a931f180b874fbd723d63a1bbe226f
SNValadez <snvaladez13@gmail.com>
2021-11-15 15:46:26 -0800

    user login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,39 @@ const router = require("express").Router();
 
 const { User } = require("../../models");
 
+
+
+router.post("/login", (req, res) => {
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(dbUsDa => {
+      if (!dbUsDa) {
+        res.status(400).json({ message: 'Cannot find user account!' });
+        return;
+      }
+  
+      const validPassword = dbUsDa.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'This password is incorrect!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.userId = dbUsDa.id;
+        req.session.username = dbUsDa.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUsDa, message: 'Currently logged in!' });
+      });
+    });
+  });
+  
+
+
+
 router.post("/", (req, res) => {
     User.create({
       username: req.body.username,
@@ -22,7 +55,8 @@ router.post("/", (req, res) => {
 
     .catch(err => {
       console.log(err);
-      
+
       res.status(500).json(err);
     });
   });
+

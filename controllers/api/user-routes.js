86ce9ba96ef48a2aadf27c14686fdda893948612
SNValadez <snvaladez13@gmail.com>
2021-11-14 22:15:35 -0800

    user post create

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,28 @@
 const router = require("express").Router();
+
 const { User } = require("../../models");
+
+router.post("/", (req, res) => {
+    User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(dbUsDa => {
+      req.session.save(() => {
+
+        req.session.userId = dbUsDa.id;
+
+        req.session.username = dbUsDa.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUsDa);
+
+      });
+    })
+
+    .catch(err => {
+      console.log(err);
+      
+      res.status(500).json(err);
+    });
+  });

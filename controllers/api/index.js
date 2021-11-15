c4f1530e13c47989ba600e6be0ddf144884751d5
SNValadez <snvaladez13@gmail.com>
2021-11-14 21:32:59 -0800

    new const index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+
+const commentRoutes = require("./comment-routes");
+
+const postRoutes = require("./post-routes");
+

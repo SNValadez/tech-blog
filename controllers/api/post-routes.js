8d936133f49a900ec777063c0c911a489bb64233
SNValadez <snvaladez13@gmail.com>
2021-11-15 16:24:18 -0800

    post delete route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,3 +5,44 @@ const { Post,
      User } = require("../../models/");
 
 const withAuth = require("../../utils/auth");
+
+
+router.delete("/:id", withAuth, (req, res) => {
+
+    Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+
+        .then((dbPoDa) => {
+            if (!dbPoDa) {
+                res.status(404).json({
+                    message: "No post associated with this id!"
+                });
+                return;
+            }
+            res.json(dbPoDa);
+        })
+
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post("/", withAuth, (req, res) => {
+    console.log("initating creation");
+
+    Post.create({
+            title: req.body.title,
+            content: req.body.post_content,
+            user_id: req.session.user_id
+        })
+        
+        .then((dbPoDa) => res.json(dbPoDa))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
